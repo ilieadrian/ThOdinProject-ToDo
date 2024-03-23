1b790a8852d2bc1a3e031bd20907a6bb7a32034a
I A <ilie.adrian1986@gmail.com>
2024-03-23 20:10:07 +0200

    updates

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -26,21 +26,6 @@ class Todo {
         return Todo.todos;
     }
 
-    // addTodo(todo) {
-    //     this.todos.push(todo);
-    // }
-
-    // getTodos() {
-    //     return this.todos;
-    // }
-
-    // setProject(project) {
-    //     this.project = project;
-    // }
-
-    // getProject() {
-    //     return this.project;
-    // }
 }
 
 class Project {
@@ -57,55 +42,21 @@ class Project {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const defaultValues  = (function () {
     const defaultProject = new Project('Default');
     let newToDo;
 
     newToDo = Todo.create('Read daily a pattern', 'https://www.patterns.dev/vanilla/import-on-interaction', '2024-03-20', 'High', 'Unfinished', 'Learing');
-    // newToDo.addTodo(newToDo);
 
     newToDo = Todo.create('Write on blog', 'Write about last part from TicTacToe game', '2024-03-21', 'Low', 'Unfinished', 'Learing');
-    // newToDo.addTodo(newToDo);
 
     newToDo = Todo.create('New Odin Chapter', 'Start a new OdinProjectChapter', '2024-04-15', 'Normal', 'Unfinished', 'Learing');
-    // newToDo.addTodo(newToDo);
 
     newToDo = Todo.create('Repair blog', 'Fix social links on blog header', '2024-03-29', 'High', 'Unfinished', 'Learing');
-    // newToDo.addTodo(newToDo);
 
     newToDo = Todo.create('TheOdinProject chapter 2', 'Finish theOdinProject chapter 2', '2024-03-15', 'Normal', 'Finished', 'Learing');
-    // newToDo.addTodo(newToDo);
 
     console.table(Todo.getAllTodos());
-    
-    // const todo1 = new Todo('Task 1', 'Description for Task 1', '2024-03-20', 'High', 'Unfinished', 'Work');
-    // const todo2 = new Todo('Task 2', 'Description for Task 2', '2024-03-21', 'Medium', 'Unfinished', 'Work');
-    
-    // defaultProject.addTodo(todo1);
-    // defaultProject.addTodo(todo2);
-
-    // todo1.setProject(project1);
-    // todo2.setProject(project1);
-
-    // console.log(todo1.getProject());
-
-    console.table(newToDo.getTodos());
 
     console.log(defaultProject)
 })();
