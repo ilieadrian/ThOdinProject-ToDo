903fe73a849ebac1e5423c0e21911f494b2bfd3f
I A <ilie.adrian1986@gmail.com>
2024-04-16 20:26:19 +0300

    work @removeItem Todo class method

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,5 +1,5 @@
 export default class Todo {
-    constructor(title, description, dueDate, priority, status, project, isChecked) {
+    constructor(title, description, dueDate, priority, status, project, isChecked, taskList) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
@@ -7,6 +7,7 @@ export default class Todo {
         this.status = status;
         this.project = project; 
         this.checked = isChecked;
+        this.taskList = taskList; // Pass taskList to the constructor
     }
 
     getProject() {
@@ -36,3 +37,43 @@ export default class Todo {
         }
     }
 }
+
+// export default class Todo {
+//     constructor(title, description, dueDate, priority, status, project, isChecked) {
+//         this.title = title;
+//         this.description = description;
+//         this.dueDate = dueDate;
+//         this.priority = priority;
+//         this.status = status;
+//         this.project = project; 
+//         this.checked = isChecked;
+//     }
+
+//     getProject() {
+//         return this.project;
+//     }
+
+//     getTitle() {
+//         return this.title;
+//     }
+
+//     setchecked(value) {
+//         this.checked = value;
+//     }
+
+//     isChecked() {
+//         return this.checked;
+//     }
+
+//     getIndex(param) {
+//         return this.taskList.findIndex(e => e.title === param);
+//     }
+
+//     removeItem(param) {
+//         const index = this.getIndex(param);
+//         if (index !== -1) {
+//             this.taskList.splice(index, 1);
+//         }
+//     }
+// }
+
