724e2f7b43f1e5ada15685851403cdfbb34f7a71
I A <ilie.adrian1986@gmail.com>
2024-04-18 22:13:04 +0300

    work @todo class

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,9 +1,6 @@
-import taskList from "./main";
-
-console.table(taskList);
 
 export default class Todo {
-    constructor(title, description, dueDate, priority, status, project, isChecked, taskList) {
+    constructor(title, description, dueDate, priority, status, project, isChecked) {
         this.title = title;
         this.description = description;
         this.dueDate = dueDate;
@@ -11,7 +8,6 @@ export default class Todo {
         this.status = status;
         this.project = project; 
         this.checked = isChecked;
-        this.taskList = taskList; // Pass taskList to the constructor
     }
 
     getProject() {
@@ -34,50 +30,15 @@ export default class Todo {
         return this.taskList.findIndex(e => e.title === param);
     }
 
-    removeItem(param) {
-        const index = this.getIndex(param);
-        if (index !== -1) {
-            this.taskList.splice(index, 1);
-        }
-    }
+    // removeItem(param) {
+    //     const index = this.getIndex(param);
+    //     if (index !== -1) {
+    //         this.taskList.splice(index, 1);
+    //     }
+    // }
 }
 
-// export default class Todo {
-//     constructor(title, description, dueDate, priority, status, project, isChecked) {
-//         this.title = title;
-//         this.description = description;
-//         this.dueDate = dueDate;
-//         this.priority = priority;
-//         this.status = status;
-//         this.project = project; 
-//         this.checked = isChecked;
-//     }
 
-//     getProject() {
-//         return this.project;
-//     }
 
-//     getTitle() {
-//         return this.title;
-//     }
 
-//     setchecked(value) {
-//         this.checked = value;
-//     }
-
-//     isChecked() {
-//         return this.checked;
-//     }
-
-//     getIndex(param) {
-//         return this.taskList.findIndex(e => e.title === param);
-//     }
-
-//     removeItem(param) {
-//         const index = this.getIndex(param);
-//         if (index !== -1) {
-//             this.taskList.splice(index, 1);
-//         }
-//     }
-// }
 
