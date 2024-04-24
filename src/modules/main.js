0671b3a4bcb743a7e81a9e6750018bc1903949aa
I A <ilie.adrian1986@gmail.com>
2024-04-24 07:09:43 +0300

    reorganised the code from main.js and startup.js to startup.js

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,40 +1,42 @@
-import '../style.css';
 // import defaultValues from './modules/startup';
 
-import Todo from './todo';
-import Project from './project';
+// import '../style.css';
+// import Todo from './todo';
+// import Project from './project';
 
-const defaultValues  = (function () {
-    const taskList = [];
-    const projectList = [];
-    let newToDo;
-    let defaultProject;
+// const defaultValues  = (function () {
+//     const taskList = [];
+//     const projectsList = [];
+//     let newToDo;
+//     let defaultProject;
 
-    defaultProject = new Project("default")
-    projectList.push(defaultProject);
+//     defaultProject = new Project("default")
+//     projectsList.push(defaultProject);
 
-    newToDo = new Todo('Read daily a pattern', 'https://www.patterns.dev/vanilla/import-on-interaction', '2024-03-20', 'High', 'Unfinished', 'Learning', false);
-    taskList.push(newToDo);
+//     newToDo = new Todo('Read daily a pattern', 'https://www.patterns.dev/vanilla/import-on-interaction', '2024-03-20', 'High', 'Unfinished', 'Learning', false);
+//     taskList.push(newToDo);
+//     // handleProject();
 
-    newToDo = new Todo('Write on blog', 'Write about last part from TicTacToe game', '2024-03-21', 'Low', 'Unfinished', 'Home', false);
-    taskList.push(newToDo);
+//     newToDo = new Todo('Write on blog', 'Write about last part from TicTacToe game', '2024-03-21', 'Low', 'Unfinished', 'Home', false);
+//     taskList.push(newToDo);
 
-    newToDo = new Todo('New Odin Chapter', 'Start a new OdinProjectChapter', '2024-04-15', 'Normal', 'Finished', 'Learning', true);
-    taskList.push(newToDo);
+//     newToDo = new Todo('New Odin Chapter', 'Start a new OdinProjectChapter', '2024-04-15', 'Normal', 'Finished', 'Learning', true);
+//     taskList.push(newToDo);
 
-    newToDo = new Todo('Repair blog', 'Fix social links on blog header', '2024-03-29', 'High', 'Unfinished', 'Learning', true);
-    taskList.push(newToDo);
+//     newToDo = new Todo('Repair blog', 'Fix social links on blog header', '2024-03-29', 'High', 'Unfinished', 'Learning', true);
+//     taskList.push(newToDo);
 
-    newToDo = new Todo('TheOdinProject chapter 2', 'Finish theOdinProject chapter 2', '2024-03-15', 'Normal', 'Finished', 'Home', false);
-    taskList.push(newToDo);
+//     newToDo = new Todo('TheOdinProject chapter 2', 'Finish theOdinProject chapter 2', '2024-03-15', 'Normal', 'Finished', 'Home', false);
+//     taskList.push(newToDo);
 
-    return {
-        taskList,
-        projectList,
-    };
+//     newToDo = new Todo('App implementation', 'Add ability to extract the projects from the array', '2024-04-24', 'Normal', 'Finished', 'Active learing', true);
+//     taskList.push(newToDo);
+
+//     return {
+//         taskList,
+//         projectsList,
+//     };
     
-})();
+// })();
 
-
-
-export default defaultValues;
+// export default defaultValues;
--- src/modules/startup.js
+++ src/modules/startup.js
@@ -1,26 +1,55 @@
-import defaultValues from './main';
+import '../style.css';
 
-const { taskList, projectList } = defaultValues;
+import Todo from './todo';
+import Project from './project';
 
-// console.table(taskList);
-// console.log(taskList[3].status);
-// taskList[3].status = false;
-// console.log(taskList[3].status);
-// taskList[1].setShecked(true);
-// console.log(taskList[1].isChecked());
-// console.table(projectList)
+const defaultValues  = (function () {
+    const taskList = [];
+    const projectsList = [];
+    let newToDo;
+    let defaultProject;
 
-function getProjectList(){
-    console.table(taskList)
-    const projetList = [...new Set(taskList.map(project => {
+    defaultProject = new Project("default")
+    projectsList.push(defaultProject);
+
+    newToDo = new Todo('Read daily a pattern', 'https://www.patterns.dev/vanilla/import-on-interaction', '2024-03-20', 'High', 'Unfinished', 'Learning', false);
+    taskList.push(newToDo);
+    // handleProject();
+
+    newToDo = new Todo('Write on blog', 'Write about last part from TicTacToe game', '2024-03-21', 'Low', 'Unfinished', 'Home', false);
+    taskList.push(newToDo);
+
+    newToDo = new Todo('New Odin Chapter', 'Start a new OdinProjectChapter', '2024-04-15', 'Normal', 'Finished', 'Learning', true);
+    taskList.push(newToDo);
+
+    newToDo = new Todo('Repair blog', 'Fix social links on blog header', '2024-03-29', 'High', 'Unfinished', 'Learning', true);
+    taskList.push(newToDo);
+
+    newToDo = new Todo('TheOdinProject chapter 2', 'Finish theOdinProject chapter 2', '2024-03-15', 'Normal', 'Finished', 'Home', false);
+    taskList.push(newToDo);
+
+    newToDo = new Todo('App implementation', 'Add ability to extract the projects from the array', '2024-04-24', 'Normal', 'Finished', 'Active learing', true);
+    taskList.push(newToDo);
+
+    return {
+        taskList,
+        projectsList,
+    };
+    
+})();
+
+const { taskList, projectsList } = defaultValues;
+
+function handleProject(){
+    const liveProjetList = [...new Set(taskList.map(project => {
         return project["project"];
     }))];
 
 
-    console.log(projetList);
+    console.log("Live Project List", liveProjetList);
 }
 
-getProjectList()
+handleProject();
 
 function getTodosByProject(taskList, project) {
     return taskList.filter(todo => todo.project === project);
@@ -34,7 +63,12 @@ const homeTodos = getTodosByProject(taskList, 'Home');
 const learningTodos = getTodosByProject(taskList, 'Learning');
 // console.log('Learning Todos:', learningTodos);
 
-
-
-
+console.table(taskList);
+console.table(projectsList)
+// console.log(taskList[3].status);
+// taskList[3].status = false;
+// console.log(taskList[3].status);
+// taskList[1].setShecked(true);
+// console.log(taskList[1].isChecked());
+// console.table(projectList)
 
