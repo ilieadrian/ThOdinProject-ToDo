c104cffe20da0a00c5cf372d184c7197cf2bedd0
I A <ilie.adrian1986@gmail.com>
2024-04-24 06:30:40 +0300

    added the function to extract the list of active projects

--- src/modules/main.js
+++ src/modules/main.js
@@ -13,16 +13,16 @@ const defaultValues  = (function () {
     defaultProject = new Project("default")
     projectList.push(defaultProject);
 
-    newToDo = new Todo('Read daily a pattern', 'https://www.patterns.dev/vanilla/import-on-interaction', '2024-03-20', 'High', 'Unfinished', 'Learing', false);
+    newToDo = new Todo('Read daily a pattern', 'https://www.patterns.dev/vanilla/import-on-interaction', '2024-03-20', 'High', 'Unfinished', 'Learning', false);
     taskList.push(newToDo);
 
     newToDo = new Todo('Write on blog', 'Write about last part from TicTacToe game', '2024-03-21', 'Low', 'Unfinished', 'Home', false);
     taskList.push(newToDo);
 
-    newToDo = new Todo('New Odin Chapter', 'Start a new OdinProjectChapter', '2024-04-15', 'Normal', 'Finished', 'Learing', true);
+    newToDo = new Todo('New Odin Chapter', 'Start a new OdinProjectChapter', '2024-04-15', 'Normal', 'Finished', 'Learning', true);
     taskList.push(newToDo);
 
-    newToDo = new Todo('Repair blog', 'Fix social links on blog header', '2024-03-29', 'High', 'Unfinished', 'Learing', true);
+    newToDo = new Todo('Repair blog', 'Fix social links on blog header', '2024-03-29', 'High', 'Unfinished', 'Learning', true);
     taskList.push(newToDo);
 
     newToDo = new Todo('TheOdinProject chapter 2', 'Finish theOdinProject chapter 2', '2024-03-15', 'Normal', 'Finished', 'Home', false);
--- src/modules/startup.js
+++ src/modules/startup.js
@@ -2,13 +2,25 @@ import defaultValues from './main';
 
 const { taskList, projectList } = defaultValues;
 
-console.table(taskList);
-console.log(taskList[3].status);
-taskList[3].status = false;
-console.log(taskList[3].status);
+// console.table(taskList);
+// console.log(taskList[3].status);
+// taskList[3].status = false;
+// console.log(taskList[3].status);
 // taskList[1].setShecked(true);
 // console.log(taskList[1].isChecked());
 // console.table(projectList)
+
+function getProjectList(){
+    console.table(taskList)
+    const projetList = [...new Set(taskList.map(project => {
+        return project["project"];
+    }))];
+
+
+    console.log(projetList);
+}
+
+getProjectList()
 
 function getTodosByProject(taskList, project) {
     return taskList.filter(todo => todo.project === project);
@@ -16,11 +28,11 @@ function getTodosByProject(taskList, project) {
 
 // Get todos for the "Home" project
 const homeTodos = getTodosByProject(taskList, 'Home');
-console.table('Home Todos:', homeTodos);
+// console.log('Home Todos:', homeTodos);
 
 // Get todos for the "Learning" project
 const learningTodos = getTodosByProject(taskList, 'Learning');
-console.log('Learning Todos:', learningTodos);
+// console.log('Learning Todos:', learningTodos);
 
 
 
