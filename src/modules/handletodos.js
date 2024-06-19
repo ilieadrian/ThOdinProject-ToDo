4851937d31bde6addc445a58580f2e7a694976bd
I A <ilie.adrian1986@gmail.com>
2024-06-19 13:31:46 +0300

    added index for intem buttons

--- src/modules/handletodos.js
+++ src/modules/handletodos.js
@@ -12,7 +12,7 @@ function handleTodos(todoList, projectsList) {
 function displayToDods(todoList){
     let ulContent = '';
     
-    todoList.forEach(element => {
+    todoList.forEach((element, index) => {
         ulContent += `
             <li class="item">
                 <div class="name-grup ${element.status ? "finished" : ''}">
@@ -22,9 +22,9 @@ function displayToDods(todoList){
                 </div>
                 <div class="action-grup ${element.status ? "finished" : ''}">
                     <p id="due-date">${element.dueDate}</p>
-                    <button class="view-btn"><img src="../src/images/view.svg" alt="" srcset=""></button>
-                    <button class="edit-btn"><img src="../src/images/edit.svg" alt="" srcset=""></button>
-                    <button><img src="../src/images/delete.svg" alt="" srcset=""></button>
+                    <button class="view-btn" id="view-btn-${index}"><img src="../src/images/view.svg" alt="" srcset=""></button>
+                    <button class="edit-btn" id="edit-btn-${index}"><img src="../src/images/edit.svg" alt="" srcset=""></button>
+                    <button id="delete-btn-${index}"><img src="../src/images/delete.svg" alt="" srcset=""></button>
                 </div>
             </li>
         `;
